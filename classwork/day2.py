45d4f311330547fb612476754a67663a6c0c7204
kailas234 <kailasnath664@gmail.com>
2025-12-31 17:35:09 +0530

    cw

---
+++ classwork/day2.py
@@ -0,0 +1,9 @@
+reciept='''
+The customer bought 2 items:
+Book Title: {0} – {1}
+Book Title: {2} – {3}
+Total Price:\t {totalprice}
+'''
+Rate= reciept.format("Python Basics", 450,"Data Science Intro", 600,totalprice=450+600)
+print(Rate.upper())
+print("\nthank".upper() + "you".upper())
